487a7fcbb0d5ae52a8d8d1d68b6d1ff2672e28f7
Olivier Philippon <olivier@rougemine.com>
2014-05-28 17:59:30 +0200

    Twitter Bootstrap test theme: switched from Grunt to gulp, better assets management
    + fixed quotes style on our app gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-var fs = require('fs');
-var ini = require('ini');
+var fs = require("fs");
+var ini = require("ini");
 // Gulp stuff
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var jshint = require('gulp-jshint');
+var gulp = require("gulp");
+var livereload = require("gulp-livereload");
+var jshint = require("gulp-jshint");
 
-var appConfig = ini.parse(fs.readFileSync('./app/config/main.ini.php', 'utf-8'));
+var appConfig = ini.parse(fs.readFileSync("./app/config/main.ini.php", "utf-8"));
 
-gulp.task('jshint', function() {
-  return gulp.src('./app/**/assets/js/**/*.js')
+gulp.task("jshint", function() {
+  return gulp.src("./app/**/assets/js/**/*.js")
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter("default"));
 });
 
-gulp.task('watch', function() {
-  var livereloadPort = parseInt(appConfig.debug['livereload.port']);
+gulp.task("watch", function() {
+  var livereloadPort = parseInt(appConfig.debug["livereload.port"]);
   var server = livereload(livereloadPort);
   gulp.watch([
-    'app/**',
-    '!**/*.log'
-  ]).on('change', function(file) {
-      console.log(file.path + ' changed.');
+    "app/**",
+    "!**/*.log"
+  ]).on("change", function(file) {
+      console.log(file.path + " changed.");
       server.changed(file.path);
   });
 });
 
-gulp.task('default', ['watch']);
+gulp.task("default", ["watch"]);
 
