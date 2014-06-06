a67565f1a22ede512e6f1902234daca968a2ac05
Olivier Philippon <olivier@rougemine.com>
2014-06-06 10:56:46 +0200

    ActionsPacker: added management of routes priorities
    * As Slim Router don't allow multiple routes with the same name, we now check the action name existence before adding it to Slim
    * Routes are now computed in a ActionsPacker plugin init "$app->before" function
    * Routes can now have a priority

--- app/boot/classes/TalkTalk/Core/Plugin/PackingBehaviour/ActionsPacker.php
+++ app/boot/classes/TalkTalk/Core/Plugin/PackingBehaviour/ActionsPacker.php
@@ -11,6 +11,49 @@ class ActionsPacker extends BasePacker
 
     protected $myConfigKey = '@actions';
 
+    public function getPackerInitCode()
+    {
+        return <<<'PLUGIN_PHP_CODE'
+namespace {
+    // Actions initialization
+    $app->vars['plugins.actions'] = array();
+    $app->vars['plugins.actions.names'] = array();
+
+    $app->before(
+        function () use ($app) {
+            $app->get('logger')->debug(
+                sprintf('Actions initialization (%d actions registered).', count($app->vars['plugins.actions']))
+            );
+
+            $actions = $app->vars['plugins.actions'];
+
+            // 1) Actions are sorted by priority
+            $actionsSorter = function (array $actionA, array $actionB)
+            {
+                $priorityA = $actionA['priority'];
+                $priorityB = $actionB['priority'];
+                if ($priorityA > $priorityB) {
+                    return -1;
+                } elseif ($priorityA < $priorityB) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            };
+            usort($actions, $actionsSorter);
+
+            // 2) Actions are registered!
+            foreach($actions as $action) {
+                call_user_func($action['actionRegistering']);
+            }
+        },
+        -10 // this "before" will be run... well... before "normal" befores
+    );
+}
+
+PLUGIN_PHP_CODE;
+    }
+
     /**
      * @inheritdoc
      */
@@ -42,26 +85,48 @@ class ActionsPacker extends BasePacker
             array($plugin->path, $actionData['target']),
             self::ACTION_FILE_PATH
         );
+        $actionPriority = isset($actionData['priority'])
+            ? (int) $actionData['priority']
+            : 0;
 
         // Advanced action settings
-        $advancedSettings = '';
+        $beforeActionDefinition = '';
+        $afterActionDefinition = '';
 
         // Route name management
         if (isset($actionData['name'])) {
-            $advancedSettings .= <<<ACTION_NAME
-    ->name('$actionData[name]')
-ACTION_NAME;
+            $actionName = $actionData['name'];
+            $beforeActionDefinition .= "
+            if (in_array('$actionName', \$app->vars['plugins.actions.names'])) {
+                \$app->get('logger')->debug('Action \"$actionName\" not added to Slim router, as this route name is already registered (plugin \"$plugin->id\").');
+                return;//Slim don't authorize named routes overriding; let's stop here!
+            }
+            ";
+            $afterActionDefinition .= "
+            \$action->name('$actionName');
+            \$app->vars['plugins.actions.names'][] = '$actionName';
+            \$app->get('logger')->debug('Action \"$actionName\" added to Slim router (plugin \"$plugin->id\").');
+            ";
         }
 
         return <<<PLUGIN_PHP_CODE
 namespace {
-    \$app->addAction('$urlPattern', function () use (\$app) {
-        \$action = \$app->includeInApp('$actionFilePath');
+    \$app->vars['plugins.actions'][] = array(
+        'actionRegistering' => function () use (\$app) {
+            $beforeActionDefinition
 
-        return call_user_func(\$action);
-    })
-    ->via('$method')
-$advancedSettings;
+            \$action = \$app->addAction('$urlPattern', function () use (\$app) {
+                \$action = \$app->includeInApp('$actionFilePath');
+
+                return call_user_func(\$action);
+            })
+            ->via('$method');
+
+            $afterActionDefinition
+        },
+        'priority' => $actionPriority
+    );
+
 }
 
 PLUGIN_PHP_CODE;
